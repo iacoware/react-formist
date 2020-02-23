d01f3b0880c1fe2aa36dc0ce7b85fe034a0ef462
Massimo Iacolare <info@massimoiacolare.it>
2020-02-23 22:46:28 +0100

    array properties: add 'primitive value arrays' test

--- tests/helpers.mapper.test.js
+++ tests/helpers.mapper.test.js
@@ -3,6 +3,20 @@ import deepmerge from "deepmerge"
 
 const head = arr => arr[0]
 const tail = arr => arr.slice(1)
+const isArrayFromName = name => !!name.match(/(\w*)\[(\d+)\]/)
+const indexFromName = name =>
+    name.match(/(?<prop>\w*)\[(?<index>\d+)\]/).groups.index
+const propFromName = name =>
+    name.match(/(?<prop>\w*)\[(?<index>\d+)\]/).groups.prop
+
+const initObjWith = (name, value) => {
+    if (isArrayFromName(name)) {
+        const propName = propFromName(name)
+        return { [propName]: [value] }
+    } else {
+        return { [name]: value }
+    }
+}
 
 const mapEntryToObj = entry => {
     const [name, value] = entry
@@ -10,11 +24,12 @@ const mapEntryToObj = entry => {
     const parts = name.split(".")
     const reversed = parts.reverse()
 
-    const deepest = { [head(reversed)]: value }
+    const deepest = initObjWith(head(reversed), value)
     const withoutDeepest = tail(reversed)
-    const result = withoutDeepest.reduce((acc, cur) => {
-        return { [cur]: acc }
-    }, deepest)
+    const result = withoutDeepest.reduce(
+        (acc, cur) => initObjWith(cur, acc),
+        deepest,
+    )
 
     return result
 }
@@ -48,5 +63,31 @@ test("many elements", () => {
 
     expect(result).toStrictEqual({
         first: { second: { third1: 42, third2: "is", third3: "the answer" } },
+    })
+})
+
+test("primitive value arrays", () => {
+    const values = {
+        "customer.addresses[0]": "5th avenue",
+        "customer.addresses[1]": "8th avenue",
+    }
+
+    const result = mapPropsToObj(values)
+
+    expect(result).toStrictEqual({
+        customer: { addresses: ["5th avenue", "8th avenue"] },
+    })
+})
+
+test.skip("custom object arrays", () => {
+    const values = {
+        "customer.addresses[0].street": "5th avenue",
+        "customer.addresses[0].city": "New York",
+    }
+
+    const result = mapPropsToObj(values)
+
+    expect(result).toStrictEqual({
+        customer: { addresses: [{ city: "New York", street: "5th avenue" }] },
     })
 })
