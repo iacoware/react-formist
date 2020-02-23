58de2aa0845c39e72a4924f5aafd7c891643b2f6
Massimo Iacolare <info@massimoiacolare.it>
2020-02-23 16:50:38 +0100

    array handling: add many elements test

--- tests/useFormist.array.test.js
+++ tests/useFormist.array.test.js
@@ -12,4 +12,20 @@ test("one element", () => {
     expect(values[0]).toBe("Fred")
 })
 
+test("many elements", () => {
+    const { result } = renderHook(() => useFormist([]))
+
+    act(() => {
+        result.current.change("[0]", "Fred")
+        result.current.change("[1]", "George")
+        result.current.change("[2]", "Nick")
+    })
+
+    const values = getValues(result)
+    expect(values.length).toBe(3)
+    expect(values).toContain("Fred")
+    expect(values).toContain("George")
+    expect(values).toContain("Nick")
+})
+
 const getValues = obj => obj.current.values
