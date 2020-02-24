e232e673e39813cd9e7970f4655860a19a8e61e7
Massimo Iacolare <info@massimoiacolare.it>
2020-02-24 21:47:57 +0100

    minor fix to example app

--- example/src/components/app.js
+++ example/src/components/app.js
@@ -29,7 +29,7 @@ const App = () => {
             <h1>Address form</h1>
             <AddressForm onSubmit={onSubmit} />
 
-            <div>{JSON.stringify(formValues, null, 2)}</div>
+            <pre>{JSON.stringify(formValues, null, 2)}</pre>
         </div>
     )
 }
