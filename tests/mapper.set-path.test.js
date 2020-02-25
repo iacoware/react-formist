bfa068978fcf44bb52d59bb8c06af33cd65cf767
Massimo Iacolare <info@massimoiacolare.it>
2020-02-25 21:47:21 +0100

    setPath: 'does not mutate original' test

---
+++ tests/mapper.set-path.test.js
@@ -0,0 +1,33 @@
+/*globals test, expect*/
+import { getPath } from "../src/mapper"
+
+const deepClone = obj => JSON.parse(JSON.stringify(obj))
+
+const setPath = (name, value, obj) => {
+    return deepClone(obj)
+}
+
+test("does not mutate original", () => {
+    const obj = { first: 42 }
+
+    const result = setPath("first", 68, obj)
+
+    expect(result === obj).toBe(false)
+})
+
+/*test("many levels", () => {
+    const obj = { first: { second: { third: 42 } } }
+
+    const result = getPath("first.second.third", obj)
+
+    expect(result === obj).toBe(false)
+})
+
+test("many levels within arrays", () => {
+    const obj = { first: { second: [{ third: 42 }, { third: 68 }] } }
+
+    const result = getPath("first.second.1.third", obj)
+
+    expect(result).toBe(68)
+})
+*/
