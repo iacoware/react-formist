b71bc344584999f317fa2c62700c0987f132ed8b
Massimo Iacolare <info@massimoiacolare.it>
2020-02-26 22:54:49 +0100

    update example

--- example/src/components/app.js
+++ example/src/components/app.js
@@ -1,23 +1,5 @@
 import React, { useState } from "react"
-import useFormist from "../../../src/useFormist"
-
-function AddressForm({ onSubmit }) {
-    const formist = useFormist(null, {
-        onSubmit,
-    })
-
-    return (
-        <form {...formist.form()}>
-            <div>
-                <input type="text" {...formist.field("firstName")} />
-            </div>
-            <div>
-                <input type="text" {...formist.field("lastName")} />
-            </div>
-            <button>Submit</button>
-        </form>
-    )
-}
+import SimpleAddressForm from "./simple-address-form"
 
 const App = () => {
     const [formValues, setFormValues] = useState({})
@@ -27,7 +9,7 @@ const App = () => {
     return (
         <div>
             <h1>Address form</h1>
-            <AddressForm onSubmit={onSubmit} />
+            <SimpleAddressForm onSubmit={onSubmit} />
 
             <pre>{JSON.stringify(formValues, null, 2)}</pre>
         </div>
---
+++ example/src/components/simple-address-form.js
@@ -0,0 +1,19 @@
+import React from "react"
+import useFormist from "../../../src/useFormist"
+
+export default function SimpleAddressForm({ onSubmit }) {
+    const initialValues = { firstName: "Kent", lastName: "Beck" }
+    const formist = useFormist(initialValues, { onSubmit })
+
+    return (
+        <form {...formist.form()}>
+            <div>
+                <input type="text" {...formist.field("firstName")} />
+            </div>
+            <div>
+                <input type="text" {...formist.field("lastName")} />
+            </div>
+            <button>Submit</button>
+        </form>
+    )
+}
