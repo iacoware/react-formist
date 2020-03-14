e652eba5b14dfe31ef0a405591c0b57d0e63985d
Massimo Iacolare <info@massimoiacolare.it>
2020-03-14 19:10:03 +0100

    review setPath tests

--- tests/mapper.set-path.array.test.js
+++ tests/mapper.set-path.array.test.js
@@ -2,7 +2,25 @@
 import { log } from "../src/helpers"
 import { setPath } from "../src/mapper"
 
-test("many levels within an array", () => {
+test("array", () => {
+    const obj = []
+
+    const result = setPath("1", 68, obj)
+
+    expect(Array.isArray(result)).toBe(true)
+    expect(result[1]).toBe(68)
+})
+
+test("array with nested object", () => {
+    const obj = []
+
+    const result = setPath("1.city", "Florence", obj)
+
+    expect(Array.isArray(result)).toBe(true)
+    expect(result[1].city).toBe("Florence")
+})
+
+test("object with nested array", () => {
     const obj = { first: { second: [{ third: 42 }, { third: 68 }] } }
 
     const result = setPath("first.second.1.third", 99, obj)
@@ -31,10 +49,10 @@ test("missing last array level", () => {
 })
 
 test("missing nested array levels", () => {
-    const obj = {}
+    const obj = []
 
-    const result = setPath("first.1.2.3", 68, obj)
+    const result = setPath("1.2.3", 68, obj)
 
-    expect(Array.isArray(result.first)).toBe(true)
-    expect(result.first[1][2][3]).toBe(68)
+    expect(Array.isArray(result)).toBe(true)
+    expect(result[1][2][3]).toBe(68)
 })
