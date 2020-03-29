e4cb9a53c0f88e86aee6a93129d1763ea07d1000
Massimo Iacolare <info@massimoiacolare.it>
2020-03-29 15:53:50 +0200

    example: refactor field

--- example/src/components/field.js
+++ example/src/components/field.js
@@ -3,18 +3,16 @@ import React from "react"
 // NOTE: it's just a styling component, it doesn't add any behavior
 
 export default function Field({ label, children }) {
-    const childrenArray = React.Children.toArray(children)
-    const inputWithStyles = addProps(childrenArray[0])
-    const validationError = childrenArray[1]
+    const input = React.Children.toArray(children)[0]
     return (
         <div className="field">
             <label className="label">{label}</label>
-            <div className="control">{inputWithStyles}</div>
-            <p className="help is-danger">{validationError}</p>
+            <div className="control">{addCssProps(input)}</div>
+            <p className="help is-danger">{input.props.error}</p>
         </div>
     )
 }
 
-function addProps(input) {
+function addCssProps(input) {
     return React.cloneElement(input, { className: "input" })
 }
